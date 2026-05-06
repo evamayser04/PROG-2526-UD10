4e1285cc91a2b905341fb05f87ff15348a1040c4
Eva <evamayser04@gmail.com>
2026-05-06 10:30:51 +0200

    commit

--- a06/A06.java
+++ a06/A06.java
@@ -1,5 +1,25 @@
 package a06;
 
-public class A06 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
+public class A06 {
+    public static void main(String[] args) {
+        Collection <Integer> colecAle = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            colecAle.add((int) (Math.random() * 100 + 1));
+        }
+        System.out.println("Coleccion aleatoria: \n" + colecAle);
+
+        Integer[] tabla = colecAle.toArray(new Integer[0]);
+
+         Arrays.sort(tabla);
+
+         Collection <Integer> colecDin = new ArrayList<>(Arrays.asList(tabla));
+
+         System.out.println("Coleccion aleatoria ordenada: \n" + colecDin);
+
+    }
 }
