7f2e7bbcd4f11923531a52c9e4b3d47c64a54b65
Eva <evamayser04@gmail.com>
2026-05-11 21:34:05 +0200

    commit

--- a08/A08.java
+++ a08/A08.java
@@ -1,5 +1,26 @@
 package a08;
 
-public class A08 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
+public class A08 {
+    public static void main(String[] args) {
+        List <String> listaA = new ArrayList<>(Arrays.asList("Ana", "Luis", "Marta", "Luis"));
+        List <String> listaB = new ArrayList<>(Arrays.asList("Ana", "Luis", "Marta", "Luis"));
+
+        System.out.println("Son iguales? " + listaA.equals(listaB));
+
+        System.out.println("Primer Luis: " + listaA.indexOf("Luis"));
+        System.out.println("Último Luis: " + listaB.lastIndexOf("Luis"));
+
+        listaA.sort(null);
+
+        System.out.println("Son iguales? " + listaA.equals(listaB));
+
+        listaB.sort(Comparator.reverseOrder());
+
+        System.out.println("Orden inverso de la listaB: " + listaB);
+    }
 }
